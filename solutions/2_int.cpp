515063451f787046a4b89ab134c30fb4053c6e4c
Your Name <you@example.com>
2024-11-25 22:12:37 +0100

    add custom int parser

--- solutions/2_int.cpp
+++ solutions/2_int.cpp
@@ -11,6 +11,112 @@ struct data {
     int max = -999;
 };
 
+int ctoi(char c){
+    switch (c){
+        case '0' : return 0;
+        break;
+        case '1' : return 1;
+        break;
+        case '2' : return 2;
+        break;
+        case '3' : return 3;
+        break;
+        case '4' : return 4;
+        break;
+        case '5' : return 5;
+        break;
+        case '6' : return 6;
+        break;
+        case '7' : return 7;
+        break;
+        case '8' : return 8;
+        break;
+        case '9' : return 9;
+        break;
+    }
+}
+
+char itoc(int i){
+    switch (i){
+        case 0 : return '0';
+        break;
+        case 1 : return '1';
+        break;
+        case 2 : return '2';
+        break;
+        case 3 : return '3';
+        break;
+        case 4 : return '4';
+        break;
+        case 5 : return '5';
+        break;
+        case 6 : return '6';
+        break;
+        case 7 : return '7';
+        break;
+        case 8 : return '8';
+        break;
+        case 9 : return '9';
+        break;
+    }
+}
+
+void char_to_int(char* c, int& i){
+    //int i;
+    i = ctoi(c[4]);
+    i = i + (ctoi(c[2]) * 10);
+    i = i + (ctoi(c[1]) * 100);
+    if (c[0] == '-') {
+        i = i * -1;
+    }
+    //return i;
+}
+
+void int_to_char(int i, char* c){
+    if(i < 0){
+        c[0] = '-';
+    } else {
+        c[0] = '0';
+    }
+    //TODO
+}
+
+// 0 1 2 3 4
+// - 9 9 . 9
+
+void pad_number(const char* in , char* out){
+    int idx = 0;
+    
+    if (in[0] == '-'){
+        out[0] = '-';
+        idx++;
+    } else {
+        out[0] = '0';
+    }
+    
+    if (in[idx+1] == '.') {
+        out[1] = '0';
+        out[2] = in[idx];
+      out[3] = '.';
+        out[4] = in[idx+2];
+    } else if (in[idx+2] == '.') {
+        out[1] = in[idx];
+        out[2] = in[idx+1];
+      out[3] = '.';
+        out[4] = in[idx+3];
+    } else if (in[idx+1] == '\0') {
+        out[1] = '0';
+        out[2] = in[idx];
+      out[3] = '.';
+        out[4] = '0';
+    } else {
+        out[1] = in[idx];
+        out[2] = in[idx+1];
+      out[3] = '.';
+        out[4] = '0';
+    }
+}
+
 int main(int argc , char* argv[] ){
     std::string name;
     std::string string_temp;
@@ -25,8 +131,15 @@ int main(int argc , char* argv[] ){
     }
     
     while ( std::getline (file,name,';') ){
+        
+        char padded_number[6];
+        int temp;
+        padded_number[5] ='\0';
+        
         getline(file,string_temp);
-        int temp = (int) (std::stof(string_temp) * 10);
+        pad_number(string_temp.c_str(),padded_number);
+        char_to_int(padded_number, temp);
+        //std::cout << padded_number << '=' << temp << '\n';
         
         data& d = map[name];
         d.sum = d.sum + temp;
