16937dc61ed7745af04c7618b1629c75cc08535d
Your Name <you@example.com>
2024-11-28 22:45:05 +0100

    add next solution try without branching

--- solutions/3_branchless.c
+++ solutions/3_branchless.c
@@ -23,9 +23,12 @@ void char_to_int(char* c, int& i){
     i = ctoi(c[4]);
     i = i + (ctoi(c[2]) * 10);
     i = i + (ctoi(c[1]) * 100);
+    /*
     if (c[0] == '-') {
         i = i * -1;
     }
+    */
+    i = i * (1 - 2 * (c[0] == '-'));
 }
 
 void int_to_char(int i, char* c){
@@ -33,12 +36,18 @@ void int_to_char(int i, char* c){
     int b10;
     int b100;
     
+    /*
     if(i < 0){
         c[0] = '-';
         i = i * -1;
     } else {
         c[0] = ' ';
     }
+    */
+    
+    c[0] = ('-' * (char) ((i >> 31) & 1)) + (' ' * (char) (1 - ((i >> 31) & 1)));
+    i = (i + (i >> 31)) ^ (i >> 31);
+    
     b1 = i % 10;
     b10 = (i % 100) / 10;
     b100 = (i % 1000) / 100;
@@ -53,36 +62,38 @@ void int_to_char(int i, char* c){
 // - 9 9 . 9
 
 void pad_number(const char* in , char* out){
-    int idx = 0;
     
+    
+    /*
     if (in[0] == '-'){
         out[0] = '-';
         idx++;
     } else {
         out[0] = '0';
     }
+    */
     
-    if (in[idx+1] == '.') {
-        out[1] = '0';
-        out[2] = in[idx];
-      out[3] = '.';
-        out[4] = in[idx+2];
-    } else if (in[idx+2] == '.') {
-        out[1] = in[idx];
-        out[2] = in[idx+1];
-      out[3] = '.';
-        out[4] = in[idx+3];
-    } else if (in[idx+1] == '\0') {
-        out[1] = '0';
-        out[2] = in[idx];
-      out[3] = '.';
-        out[4] = '0';
-    } else {
-        out[1] = in[idx];
-        out[2] = in[idx+1];
-      out[3] = '.';
-        out[4] = '0';
-    }
+    out[0] = ((in[0] == '-') * '-'); // else is irrelevant
+    int idx = 0;
+    idx = 1 * (in[0] == '-');
+    
+    out[3] = '.';
+    
+    const char& in0 = in[idx];
+    const char& in1 = in[idx+1];
+    const char& in2 = in[idx+2];
+    const char& in3 = in[idx+3];
+    
+    
+    out[1] = (char) ('0' * (( in1 == '.' ) + ( in1 == '\0' ))) + (in0 * (( in2 == '.' ) + ( in2 == '\0' )));
+    out[2] = (char) (in0 * (( in1 == '.' ) + ( in1 == '\0' ))) + (in1 * (( in2 == '.' ) + ( in2 == '\0' )));
+    out[4] = (char) ('0' * (in1 == '\0')) + (in2 *  ( in1 == '.' ))  + (in3 *  ( in2 == '.' ));
+    
+    /*
+    out[1] = ('0' * (in1 == '.' || in1 == '\0')) + (in0 * (in1 != '.' && in1 != '\0'));
+    out[2] = (in0 * (in1 == '.' || in1 == '\0')) + (in1 * (in2 == '.' || in2 == '\0'));
+    out[4] = ('0' * (in1 == '\0')) + (in2 * (in1 == '.')) + (in3 * (in2 == '.'));
+    */
 }
 
 int main(int argc , char* argv[] ){
